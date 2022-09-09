4fc34adf07c56855d4bdd9d84f77cb52c63f6bef
Miguel Belo <belo_miguel@hotmail.com>
2022-09-09 14:01:57 +0100

    changing on ex1 char to char decimal value

--- projects/C/c_00/ex01/ft_print_alphabet.c
+++ projects/C/c_00/ex01/ft_print_alphabet.c
@@ -6,27 +6,29 @@
 /*   By: josmigue <josmigue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 13:05:22 by josmigue          #+#    #+#             */
-/*   Updated: 2022/09/09 12:53:14 by josmigue         ###   ########.fr       */
+/*   Updated: 2022/09/09 14:01:05 by josmigue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_putchar(char a)
+{
+	write(1, &a, 1);
+}
+
 void	ft_print_alphabet(void)
 {
 	char	l;
 
-	l = 'a' ;
-	while (l <= 'z')
+	l = 97 ;
+	while (l <= 122)
 	{
-		write(1, &l, 1);
+		ft_putchar(l);
 		l++;
 	}
-	return (0);
 }
-
 /* int main (void)
 {
 	ft_print_alphabet();
-	return 0;
 } */
