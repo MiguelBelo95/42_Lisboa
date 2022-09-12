332e928bddb8ebd96566e5e27b7d91717155bcb4
Jose Miguel Belo Pereira Francisco <josmigue@c1r17s6.42lisboa.com>
2022-09-12 20:20:47 +0100

    adding ex07. TBD again

---
+++ projects/C/c_00/ex07/mb_ft_putnbr.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mb_ft_putnbr.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josmigue <josmigue@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/12 19:25:47 by josmigue          #+#    #+#             */
+/*   Updated: 2022/09/12 19:43:53 by josmigue         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void    ft_putnbr(int num)
+{
+    int res;
+    char *str;
+    int count;
+
+    res = num;
+    while( res / 10 > 10) //while we haven't arrive to units
+
+
+    
+}
