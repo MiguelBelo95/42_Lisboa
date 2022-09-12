8c2f559f0aec34efd8e39de9f552af933020c65e
Jose Miguel Belo Pereira Francisco <josmigue@c1r17s6.42lisboa.com>
2022-09-12 19:21:47 +0100

    Adding final ex06 w normitte compliance

--- projects/C/c_00/ex06/ft_print_comb2.c
+++ projects/C/c_00/ex06/ft_print_comb2.c
@@ -3,65 +3,52 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_comb2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: josmigue <josmigue@student.42lisboa.com>   +#+  +:+       +#+        */
+/*   By: josmigue <josmigue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/12 14:32:17 by josmigue          #+#    #+#             */
-/*   Updated: 2022/09/12 18:17:46 by josmigue         ###   ########.fr       */
+/*   Created: 2022/09/09 12:03:35 by jabecass          #+#    #+#             */
+/*   Updated: 2022/09/12 19:20:00 by josmigue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
 
-//function that writes combo ft_printcombo
-void	ft_combwriter(int a, int b)
+void	ft_putchar(char c, char d)
 {
-	char a_01;
-	a_01 = a / 10 + 48;
-	char a_02;
-	a_02= a % 10 + 48;
-	char b_01;
-	b_01 = b / 10 + 48;
-	char b_02;
-	b_02 = b % 10 + 48;
-
-	write(1, &a_01, 1);
-	write(1, &a_02, 1);
-	write(1, " ", 1);
-	write(1, &b_01, 1);
-	write(1, &b_02, 1);
-	if( a < 98)
-	{
-		write(1, ",  ", 2);
-	}
+	write(1, &c, 1);
+	write(1, &d, 1);
 }
 
-//function that iterates through numbers
 void	ft_print_comb2(void)
 {
-	int a;
-	int b;
+	int	a;
+	int	b;
 
 	a = 0;
-
 	while (a <= 98)
 	{
 		b = a + 1;
-
 		while (b <= 99)
 		{
-			ft_combwriter(a, b);
+			ft_putchar(a, b);
 			b++;
 		}
 		a++;
 	}
 }
 
+void	ft_int_to_str(int a, int b)
+{
+	ft_putchar((48 + a / 10), (48 + a % 10));
+	write(1, " ", 1);
+	ft_putchar((48 + b / 10), (48 + b % 10));
+	if (a != 98)
+	{
+		write(1, ", ", 2);
+	}
+}
 
-
-//function main that calls iterator
-int main(void)
+int	main(void)
 {
 	ft_print_comb2();
-	return(0);
+	return (0);
 }
