a256af6f9bdc0e8d9563bd8a462159edd5ba60c7
Jose Miguel Belo Pereira Francisco <josmigue@c1r17s6.42lisboa.com>
2022-09-15 17:47:47 +0100

    adding ex06 w norminette

---
+++ projects/C/c_01/ex06/ft_strlen.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josmigue <josmigue@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/15 17:40:30 by josmigue          #+#    #+#             */
+/*   Updated: 2022/09/15 17:46:17 by josmigue         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/*int main(void)
+{
+    char *str;
+    str = "My name is Jeff!";
+
+    printf("%d\n", ft_strlen(str));
+    return(0);
+}*/
