7c3984fa6bdc63be71194be0ec428ea064b56818
Jose Miguel Belo Pereira Francisco <josmigue@c1r17s2.42lisboa.com>
2022-09-18 20:47:18 +0100

    adding ex05 w norm compliance

---
+++ projects/C/c_02/ex05/mb_str_uppercase.c
@@ -0,0 +1,17 @@
+    #include <stdio.h>
+
+
+    int ft_str_is_uppercase(char *str)
+    {
+        int i = 0;
+
+        if(str[i] == '\0')
+            return (1);
+        while(str[i] != '\0')
+        {
+            if(!(str[i] >= 'A' && str[i] <= 'Z'))
+                return (0);
+            i++;
+        }
+        return(1);
+    }
