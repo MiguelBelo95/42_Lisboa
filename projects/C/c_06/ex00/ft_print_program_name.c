2569119573dfdc86ba963d977dfbb7113b4bd172
Jose Miguel Belo Pereira Francisco <josmigue@c1r17s6.42lisboa.com>
2022-09-26 17:31:31 +0100

    add ex00 c06 w newline

--- projects/C/c_06/ex00/ft_print_program_name.c
+++ projects/C/c_06/ex00/ft_print_program_name.c
@@ -6,7 +6,7 @@
 /*   By: josmigue <josmigue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 12:20:52 by josmigue          #+#    #+#             */
-/*   Updated: 2022/09/22 12:34:16 by josmigue         ###   ########.fr       */
+/*   Updated: 2022/09/26 17:29:54 by josmigue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@ int	main(int argc, char *argv[])
 	int	len;
 
 	len = 0;
-	while (argv[argc - 1][len] != '\0')
+	while (argv[0][len] != '\0' && argc)
 	{
 		len++;
 	}
-	write(1, argv[argc - 1], len);
+	write(1, argv[0], len);
+	write(1, "\n", 1);
 	return (0);
 }
