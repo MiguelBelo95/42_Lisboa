70e80f0bfde99bb83099c637858bc82c795a46e8
Jose Miguel Belo Pereira Francisco <josmigue@c1r18s9.42lisboa.com>
2022-09-26 20:51:46 +0100

    adjusting error on ex02

--- projects/C/c_07/ex02/ft_ultimate_range.c
+++ projects/C/c_07/ex02/ft_ultimate_range.c
@@ -6,7 +6,7 @@
 /*   By: josmigue <josmigue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/24 15:37:54 by josmigue          #+#    #+#             */
-/*   Updated: 2022/09/25 15:00:14 by josmigue         ###   ########.fr       */
+/*   Updated: 2022/09/26 20:25:06 by josmigue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,13 @@ int	ft_ultimate_range(int **range, int min, int max)
 		return (0);
 	}
 	*range = (int *)malloc(sizeof(int) * (max - min));
+	if (*range == NULL)
+		return (-1);
 	while (min < max)
 	{
-		range[0][i++] = min++;
+		(*range)[i] = min;
+		i++;
+		min++;
 	}
-	if (!(*range))
-		return (-1);
 	return (i);
 }
