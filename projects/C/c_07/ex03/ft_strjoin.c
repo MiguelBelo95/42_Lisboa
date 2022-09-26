dbf2c9b9bb5579aab87570a749488b5f05b9dae0
Jose Miguel Belo Pereira Francisco <josmigue@c1r17s6.42lisboa.com>
2022-09-26 18:19:57 +0100

    add ex03 w norm

--- projects/C/c_07/ex03/ft_strjoin.c
+++ projects/C/c_07/ex03/ft_strjoin.c
@@ -6,40 +6,43 @@
 /*   By: josmigue <josmigue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 15:01:23 by josmigue          #+#    #+#             */
-/*   Updated: 2022/09/25 19:12:58 by josmigue         ###   ########.fr       */
+/*   Updated: 2022/09/26 18:14:55 by josmigue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
 
-
-int ft_strlen(char *str)
-{
-	int	i;
-	
-	i = 0;
-	while (str[i])
-	{
-		i++;
-		printf("%c", str[i]);
-	}
-	return (i);
-}
+char	*ft_strcat(char *dest, char *src);
+int		ft_strlen(int size, char *str);
+int		ft_strs_char_counter(int size, char **strs);
 
 char	*ft_strjoin(int size, char **strs, char *sep)
 {
-	//I need a strs_char_counter function
+	char	*res;
+	int		i;
+	int		n_chars;
 
-	//I need a strlen function, both for strs[i] and sep
+	if (size == 0)
+		return ((char *)malloc(sizeof(char)));
+	i = 0;
+	n_chars = ft_strs_char_counter(size, strs) + ft_strlen(size, sep) + 1;
+	res = (char *)malloc(sizeof(char) * n_chars);
+	res[0] = 0;
+	while (strs[i])
+	{
+		ft_strcat(res, strs[i]);
+		i++;
+		if (i < size)
+			ft_strcat(res, sep);
+	}
+	return (res);
+}
 
-	//I need a str_cat function to appen strs[i] with sep
-
-
-char *ft_strcat(char *dest, char *src)
+char	*ft_strcat(char *dest, char *src)
 {
 	int	i;
-	int counter;
+	int	counter;
 
 	i = 0;
 	counter = 0;
@@ -52,4 +55,40 @@ char *ft_strcat(char *dest, char *src)
 	}
 	dest[counter + i] = '\0';
 	return (dest);
+}
+
+int	ft_strlen(int size, char *str)
+{
+	int	i;
+	int	n_sep;
+	int	c_counter;
+
+	c_counter = 0;
+	i = 0;
+	n_sep = size - 1;
+	while (str[i])
+		i++;
+	c_counter = i * n_sep;
+	return (c_counter);
+}
+
+int	ft_strs_char_counter(int size, char **strs)
+{
+	int	res;
+	int	i;
+	int	j;
+
+	res = 0;
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (strs[i][j])
+		{
+			res++;
+			j++;
+		}
+		i++;
+	}
+	return (res);
 }
